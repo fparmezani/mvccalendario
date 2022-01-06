f20ad66d794b1633bc72d63f2ce1a8093bfc52ff
Fernando Parmezani dos Reis <tetris.freis@terceiros.mag.com.br>
2022-01-06 18:39:06 -0300

    Commit

---
+++ MvcCalendario/src/MvcCalendario.App/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MvcCalendario.Business.Interfaces;
+using MvcCalendario.Business.Notifications;
+using MvcCalendario.Business.Services;
+using MvcCalendario.Data.Context;
+using MvcCalendario.Data.Repository;
+
+namespace MvcCalendario.App.Configurations
+{
+    public static class DependencyInjectionConfig
+    {
+
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<MvcContext>();
+            services.AddScoped<IClienteRepository, ClienteRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+
+
+            services.AddScoped<INotificador, Notificador>();
+            services.AddScoped<IClienteService, ClienteService>();
+
+
+            return services;
+        }
+
+    }
+}
